cd6b300b84c2f19acc77d802c1395e1cc8984521
Azerpas <19282069+azerpas@users.noreply.github.com>
2017-09-05 22:46:44 +0200

    1.1 commit
    Added color exception, will search through database and delete color links.
    Now saving to .json file with "hour/minute.json" format

--- urlscraper.py
+++ urlscraper.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# @azerpas 
+# @azerpas on IG // @azrpas on Twitter
 
 
 import BeautifulSoup
@@ -8,7 +8,7 @@ import requests
 import json
 import os , time , random , datetime , threading
 
-
+colors = ["Black" , "Blue" , "Red" , "Off White" , "Navy" , "Multi" ,"Teal" , "Bright Yellow" , "Yellow Plaid" , "Olive Drab", "Dark Red" , "Magenta" , "White" , "Pink" , "Woodbine" , "Light Yellow" , "Light Burgundy" , "Denim" , "Green" , "Royal" , "Pale Peach" , "Heater Grey","Heather Mustard" , "Navy Digi Camo" , "Dark Purple" , "Orange" , "Rose" , "Ice Blue" , "Plum" , "Pale Lime" , "Bright Orange", "Hickory Stripe", "Washed Blue", "Peach", "Tan Digi Camo", "Pink Digi Camo", "pale violet", "indigo", "gold", "dark olive","navy digi camo","emerald green","silver","dusty teal","rigid indigo","yellow","brown","desrt camo","olive","charcoal","pine","burgundy","purple","maroon","snakeskin","rust","woodland camo","oxblood","ash grey","khaki","tan"]
 category = ["accessories","jackets","shirts","tops_sweaters","sweatshirts","pants","hats","bags","shoes","skate"]
 
 base_url = "http://www.supremenewyork.com/shop/all/"
@@ -32,18 +32,24 @@ def main():
 		t = threading.Thread(target=retrieveURL)
 		t.daemon = True
 		t.start()
-		print("Thread n°" + str(i) + " launched")
+		log("Thread n°" + str(i) + " launched")
 		time.sleep(random.uniform(3.4,5.2))
 	while not active_threads == 0:
-		print('Active Threads ----..---- ' + str(active_threads))
+		log('Active Threads ----..---- ' + str(active_threads))
 		time.sleep(5)
 
+	log("Ended")
 	print(items)
+
+def log(event):
+    print('SupremeScrap by Azerpas :: ' + str(datetime.datetime.now().strftime('%H:%M:%S')) + ' :: ' + str(event))
+
 
 def retrieveURL():
 	soCount = 0
 	global active_threads
 	active_threads += 1
+	d = datetime.datetime.now().strftime('%H:%M')
 
 	for i in category:
 		s = requests.session()
@@ -56,19 +62,33 @@ def retrieveURL():
 				if "sold out" in name:
 					soCount += 1 #FUN
 				else:
+					for key in colors:
+						if key.lower() in name:
+							name = 'error'
 					link = ka['href']
-					print("Item is: " + name.encode('utf-8') + " " + link.encode('utf-8'))
+					log("Item is: " + name.encode('utf-8') + " " + link.encode('utf-8'))
 					items[name] = str(link)
+					dicttt = {name:link}
+					try:
+						with open(str(d)+'.json') as f:
+							data = json.load(f)
+							data.update(dicttt)
+						with open(str(d)+'.json','w') as f:
+							json.dump(data,f)
+					except IOError:
+						with open(str(d)+'.json','w') as f:
+							json.dump(dicttt,f)
 		print('\n\n')
 		print("----------------------------")
 		print("----------------------------")
-		print("Sleeping 3 seconds")
+		print("Sleeping 0 seconds")
 		print("----------------------------")
 		print("----------------------------")
 		print('\n\n')
-		time.sleep(3)
+		time.sleep(0)
 	active_threads -= 1
-	print("Sold Out Counter = " + str(soCount))
+	log("Sold Out Counter = " + str(soCount))
+	log("JSON SAVED IN: " + str(d) + ".json")
 
 if __name__ == "__main__":
     main()
